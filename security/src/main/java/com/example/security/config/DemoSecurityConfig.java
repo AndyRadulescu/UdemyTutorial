910801bb2c8815addfd364b93fd05fe80d97152e
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2022-10-23 11:33:59 +0200

    add custom login page

--- security/src/main/java/com/example/security/config/DemoSecurityConfig.java
+++ security/src/main/java/com/example/security/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +11,17 @@ import org.springframework.security.core.userdetails.User;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/showLoginPage")
+                .loginProcessingUrl("/authenticateUser")
+                .permitAll();
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         User.UserBuilder users = User.withDefaultPasswordEncoder();
 
@@ -17,5 +29,7 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("Andy").password("test123").roles("EMPLOYEE"))
                 .withUser(users.username("Diana").password("test123").roles("MANAGER"))
                 .withUser(users.username("Andu").password("test123").roles("ADMIN"));
+
+
     }
 }
